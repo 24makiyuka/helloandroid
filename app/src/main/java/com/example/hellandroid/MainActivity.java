0584441f1d9b82c104b53f19cda2eb8e391a1612
24makiyuka <70732444+24makiyuka@users.noreply.github.com>
2021-09-27 01:32:06 +0900

    commit

--- app/src/main/java/com/example/hellandroid/MainActivity.java
+++ app/src/main/java/com/example/hellandroid/MainActivity.java
@@ -9,15 +9,17 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
     }
 
     public void onClick(View view) {
         ImageView imageView = findViewById(R.id.imageView);
         imageView.setImageResource(R.drawable.flower);
-        Toast.makeText(this, "안녕하세요 버튼 클릭입니다", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "이미지를 보여줍니다", Toast.LENGTH_SHORT).show();
     }
 }
